018aea8072be75e5d54cecb8245ada5a020f075e
Bassel Yazbek <103540209+BasselYazbek@users.noreply.github.com>
2022-10-16 12:12:31 +0300

    Add files via upload

---
+++ LabWork 3 in Csharp/LabWork 3 in Csharp/Goods.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+
+namespace LabWork_3_in_Csharp
+{
+    // Class should be public to make the Xml serialization
+    [Serializable()]
+    public class Goods : ISerializable
+    {
+        private int code;
+        private string name;
+        private string manufactured;
+        private double cost;
+        private int quantity;
+
+        // We should create this constructor in order to make the Xml Serialization
+        public Goods() { }
+
+        public Goods(int code, string name, string manufactured, double cost, int quantity)
+        {
+            this.code = code;
+            this.name = name;
+            this.manufactured = manufactured;
+            this.cost = cost;
+            this.quantity = quantity;
+        }
+
+        public int Code { get; set; }
+        public string Name { get; set; }
+        public string Manufactured { get; set; }
+        public double Cost{ get; set; }
+        public int Quantity { get; set; }
+
+        // Calculate total cost of the batch
+        public double TotalBatchCost()
+        {
+            double sum = 1.0;
+            if(quantity >= 0)
+            {
+                sum = cost * quantity;
+            }
+            return sum;
+        }
+
+        // Increase total cost by 15%
+        public double IncreasePercentage()
+        {
+            double resultt = 1.0;
+            double costIncrease = 0.15;
+            if(cost > 0)
+            {
+                resultt = cost + (cost * costIncrease);
+            }
+            return resultt;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("The item code is: {0}, it's name is: {1}, " +
+                "this item is Manufactured in {2}, it's price is: {3} " +
+                " and the quantity in the stock is :{4}", code, name, manufactured, cost, quantity);
+        }
+
+        // Binary Serialization
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("Code", code);
+            info.AddValue("Name", name);
+            info.AddValue("Manufactured", manufactured);
+            info.AddValue("Cost", cost);
+            info.AddValue("Quantity", quantity);
+        }
+
+        public Goods(SerializationInfo info, StreamingContext context)
+        {
+            code = (int)info.GetValue("Code", typeof(int));
+            name = (string)info.GetValue("Name", typeof(string));
+            manufactured = (string)info.GetValue("Manufactured", typeof(string));
+            cost = (double)info.GetValue("Cost", typeof(double));
+            quantity = (int)info.GetValue("Quantity", typeof(int));
+        }
+
+
+        
+    }
+}
---
+++ LabWork 3 in Csharp/LabWork 3 in Csharp/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace LabWork_3_in_Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Goods[] goods;
+            int arraySize = 5;
+
+            goods = new Goods[arraySize];
+            for (int i = 0; i < arraySize; i++)
+            {
+                goods[0] = new Goods(5614648, "Galaxy chocolate", "Switherland", 3.62, 389);
+                goods[1] = new Goods(45679871, "Milka Bar", "Germany", 4.85, 238);
+                goods[2] = new Goods(789547841, "Nutella Go!", "Italy", 2.84, 436);
+                goods[3] = new Goods(542123874, "Reese's chocolate", "USA", 7.34, 253);
+                goods[4] = new Goods(84456717, "Sport Ritter", "Finland", 4.38, 207);
+                Console.WriteLine(goods[i]);
+            }
+            Console.WriteLine("===================================");
+            
+            Console.WriteLine("The total cost of the batch of goods is: ");
+            foreach (Goods a in goods)
+            {
+                Console.WriteLine(a.TotalBatchCost());
+            }
+            Console.WriteLine("========================================");
+            Console.WriteLine("The new prices after we add 15% on the prices will be: ");
+
+            foreach (Goods a in goods)
+            {
+                Console.WriteLine(a.IncreasePercentage());
+            }
+            Console.WriteLine("=======================================");
+
+
+            
+            // Binary Serialize
+            Stream stream = File.Open("goodsData.dat", FileMode.Create);           
+            BinaryFormatter bf = new BinaryFormatter();
+
+            bf.Serialize(stream, goods);
+            stream.Close();
+
+            goods = null;
+            stream = File.Open("GoodsData.dat", FileMode.Open);
+            bf = new BinaryFormatter();
+            goods = (Goods[])bf.Deserialize(stream);
+            foreach (Goods a in goods)
+            {
+                Console.WriteLine(a.ToString());
+            }
+            stream.Close();
+            
+
+            // The Xml Serialization & Deserialization
+            Console.WriteLine("=================================");
+            Console.WriteLine("The Xml Serialization is lready done.");
+
+            using (Stream fs = new FileStream(@"C:\Users\Public\XmlGoodsData.xml", FileMode.Create, FileAccess.Write))
+            {
+                XmlSerializer serializer2 = new XmlSerializer(typeof(Goods[]));
+                serializer2.Serialize(fs, goods);
+            }
+            goods = null;
+            XmlSerializer serializer3 = new XmlSerializer(typeof(Goods[]));
+            using(FileStream fs2= File.OpenRead(@"C:\Users\Public\XmlGoodsData.xml"))
+            {
+                goods = (Goods[])serializer3.Deserialize(fs2);
+            }
+         
+            
+            Console.WriteLine("===============================");
+            Console.WriteLine("Json Serialize & Deserialize is done.");
+
+            // JSON Serializing
+            string filename = @"C:\Users\Public\JsonGoodsData.xml";
+            var json = Newtonsoft.Json.JsonConvert.SerializeObject(goods);
+            if(System.IO.File.Exists(filename)== false)
+            {
+                System.IO.File.WriteAllText(filename, json);
+            }
+            else
+            {
+                System.IO.File.Delete(filename);
+                System.IO.File.WriteAllText(filename, json);
+            }
+
+            Goods[] c2 = Newtonsoft.Json.JsonConvert.DeserializeObject<Goods[]>(json);
+
+            Console.ReadLine();
+        }
+    }
+}
